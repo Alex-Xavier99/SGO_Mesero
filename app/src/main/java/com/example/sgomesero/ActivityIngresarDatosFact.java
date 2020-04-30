38bf3fabb790e236ca292fb992e75a119dc8ea3a
AO <alexortizperugachi@gmail.com>
2020-04-30 00:16:25 -0500

    Se crearon las activitys de mesa, tipo de orden

--- app/src/main/java/com/example/sgomesero/ActivityIngresarDatosFact.java
+++ app/src/main/java/com/example/sgomesero/ActivityIngresarDatosFact.java
@@ -19,23 +19,25 @@ public class ActivityIngresarDatosFact extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ingresar_datos_fact);
 
-        subtitle = (TextView)findViewById(R.id.txtview_nromesa);
-        razonsocial = (EditText)findViewById(R.id.txt_razonsocial);
-        cedula = (EditText)findViewById(R.id.txt_cedula);
-        direccion = (EditText)findViewById(R.id.txt_direccion);
-        telefono = (EditText)findViewById(R.id.txt_telefono);
-        correo = (EditText)findViewById(R.id.txt_correo);
+        subtitle = (TextView)findViewById(R.id.txtview_nromesa);//Subtitulo de la activity
+        razonsocial = (EditText)findViewById(R.id.txt_razonsocial);//Servira para ingresar leer los datos de razon social
+        cedula = (EditText)findViewById(R.id.txt_cedula);//Servira para leer el dato de cedula
+        direccion = (EditText)findViewById(R.id.txt_direccion);//Servira para leer los datos ingresados de la direccion
+        telefono = (EditText)findViewById(R.id.txt_telefono);//Servira para leer los datos del telefono
+        correo = (EditText)findViewById(R.id.txt_correo);//Servira para leer los datos del correo
 
         subti = getIntent().getStringExtra("m1");
         subtitle.setText(subti);
     }
 
+    //Metodo para ingresar las ordenes
     public void IngresarOrden(View view){
         Intent orden = new Intent(this,ActivityTipoOrden.class);
-        orden.putExtra("m1",subti);
+        orden.putExtra("m1",subti);//Se envia el nombre de la mesa a la siguiente activity
         startActivity(orden);
         finish();
     }
+    //Metodo en el caso de aplastar en el boton de regreso
     @Override
     public  void onBackPressed(){
         Intent principal = new Intent(this,MainActivity.class);
