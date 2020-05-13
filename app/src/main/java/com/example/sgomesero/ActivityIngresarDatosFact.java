2bd3a40aa1a1bc83085a3c173f0b4a3cc11c0752
Christian Vicente <58301860+chrisvicenter@users.noreply.github.com>
2020-05-12 23:44:53 -0500

    Se creo la conexión con la base de datos, en la tabla cliente.

--- app/src/main/java/com/example/sgomesero/ActivityIngresarDatosFact.java
+++ app/src/main/java/com/example/sgomesero/ActivityIngresarDatosFact.java
@@ -7,6 +7,18 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.androidnetworking.AndroidNetworking;
+import com.androidnetworking.common.Priority;
+import com.androidnetworking.error.ANError;
+import com.androidnetworking.interfaces.JSONObjectRequestListener;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class ActivityIngresarDatosFact extends AppCompatActivity {
 
@@ -24,6 +36,64 @@ public class ActivityIngresarDatosFact extends AppCompatActivity {
         telefono = (EditText)findViewById(R.id.txt_telefono);//Servira para leer los datos del telefono
         correo = (EditText)findViewById(R.id.txt_correo);//Servira para leer los datos del correo
     }
+
+    public void CrearCliente(View view){
+        ingresarCliente();
+    }
+
+    private void ingresarCliente(){
+        if(isValidarCampos()){
+
+            String rzso = razonsocial.getText().toString();
+            String cdla = cedula.getText().toString();
+            String dire = direccion.getText().toString();
+            String telf = telefono.getText().toString();
+            String corr = correo.getText().toString();
+
+
+            Map<String,String> datos = new HashMap<>();
+            datos.put("cli_ci",cdla);
+            datos.put("cli_nom",rzso);
+            datos.put("cli_dir",dire);
+            datos.put("cli_email",corr);
+            datos.put("cli_telf",telf);
+            JSONObject jsonData = new JSONObject(datos);
+
+            AndroidNetworking.post("https://safe-bastion-34410.herokuapp.com/api/cliente")
+                    .addJSONObjectBody(jsonData)
+                    .setPriority(Priority.MEDIUM)
+                    .build()
+                    .getAsJSONObject(new JSONObjectRequestListener() {
+                        @Override
+                        public void onResponse(JSONObject response) {
+                            try {
+                                String estado = response.getString("estado");
+                                String error = response.getString("error");
+                                Toast.makeText(ActivityIngresarDatosFact.this, estado, Toast.LENGTH_SHORT).show();
+                                //Toast.makeText(GuardarProductoActivity.this, "Error: "+error, Toast.LENGTH_SHORT).show();
+                            } catch (JSONException e) {
+                                Toast.makeText(ActivityIngresarDatosFact.this, "Error: "+e.getMessage(), Toast.LENGTH_SHORT).show();
+                            }
+                        }
+
+                        @Override
+                        public void onError(ANError anError) {
+                            Toast.makeText(ActivityIngresarDatosFact.this, "Error: "+anError.getErrorDetail(), Toast.LENGTH_SHORT).show();
+                        }
+                    });
+        }else{
+            Toast.makeText(this, "No se puede insertar un cliente si existen campos vacios", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private boolean isValidarCampos(){
+        return !razonsocial.getText().toString().trim().isEmpty() &&
+                !cedula.getText().toString().trim().isEmpty() &&
+                !direccion.getText().toString().trim().isEmpty() &&
+                !telefono.getText().toString().trim().isEmpty() &&
+                !correo.getText().toString().trim().isEmpty();
+    }
+
     public void DatosFactura(View view){
         Intent verificarorden = new Intent(this, ActivityVerificarOrden.class);
         startActivity(verificarorden);
