edbc01cc5094fc715539510aae3ed89466731dec
Christian Vicente <58301860+chrisvicenter@users.noreply.github.com>
2020-05-25 23:42:02 -0500

    Se modificó el login, ahora se autentica con la base de datos. Se creó el alert de carga. El spnier de mesas ya no usa datos quemados.

--- app/src/main/java/com/example/sgomesero/ActivityLogin.java
+++ app/src/main/java/com/example/sgomesero/ActivityLogin.java
@@ -8,11 +8,24 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.androidnetworking.AndroidNetworking;
+import com.androidnetworking.common.Priority;
+import com.androidnetworking.error.ANError;
+import com.androidnetworking.interfaces.JSONObjectRequestListener;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class ActivityLogin extends AppCompatActivity {
 
     //Variables para user y password
     EditText txt_user;
     EditText txt_pasw;
+
+    final LoadingDialog loadingDialog = new LoadingDialog(ActivityLogin.this);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,38 +37,70 @@ public class ActivityLogin extends AppCompatActivity {
         txt_pasw = (EditText)findViewById(R.id.editTextPassword);
     }
 
-    //Función que valida usuario y contraseña
-    public boolean Validar(){
-        boolean esVal = false;
-        String str_user = txt_user.getText().toString();
-        String str_pasw = txt_pasw.getText().toString();
 
-        txt_user.setText("");
-        txt_pasw.setText("");
+    private void ingresarUser(String user, String pasw){
+            Map<String,String> datos = new HashMap<>();
+            datos.put("email",user);
+            datos.put("password",pasw);
+            JSONObject jsonData = new JSONObject(datos);
 
-        //Ingresar método de consulta
-        //public String Consulta(str_user){}
+            AndroidNetworking.post("https://safe-bastion-34410.herokuapp.com/api/login")
+                    .addJSONObjectBody(jsonData)
+                    .setPriority(Priority.MEDIUM)
+                    .build()
+                    .getAsJSONObject(new JSONObjectRequestListener() {
+                        @Override
+                        public void onResponse(JSONObject response) {
+                            try {
 
-        if(str_pasw.equals("clave")){
-            esVal = true;
-        }
+                                String mensaje = response.getString("message");
+                                Toast.makeText(ActivityLogin.this, mensaje, Toast.LENGTH_SHORT).show();
 
-        return esVal;
+                                if(mensaje.equals("Bienvenido")){
+                                    IrSguienteActivity();
+                                }
+                            } catch (JSONException e) {
+                                Toast.makeText(ActivityLogin.this, "Error1: "+e.getMessage(), Toast.LENGTH_SHORT).show();
+                                loadingDialog.dismissDialog();
+                            }
+                        }
+
+                        @Override
+                        public void onError(ANError anError) {
+                            Toast.makeText(ActivityLogin.this, "Error2: "+anError.getErrorDetail(), Toast.LENGTH_SHORT).show();
+                            loadingDialog.dismissDialog();
+                        }
+                    });
     }
 
-    public void Ingresar(View view){
 
-        if(Validar()){
-            IrSguienteActivity();
-        } else{
-            Toast.makeText(this, "Usuario o contraseña no válidos, pruebe otra vez", Toast.LENGTH_SHORT).show();
+    //Función que valida usuario y contraseña
+    public void Validar(View view){
+        if(isValidarCampos()) {
+            String str_user = txt_user.getText().toString();
+            String str_pasw = txt_pasw.getText().toString();
+
+            txt_user.setText("");
+            txt_pasw.setText("");
+
+            loadingDialog.startLoadingDialog();
+
+            ingresarUser(str_user, str_pasw);
+        } else {
+            Toast.makeText(this, "No puede dejar los campos vacíos", Toast.LENGTH_SHORT).show();
         }
+    }
+
+    private boolean isValidarCampos(){
+        return !txt_user.getText().toString().trim().isEmpty() &&
+                !txt_pasw.getText().toString().trim().isEmpty();
     }
 
     public void IrSguienteActivity(){
         Intent actSelMes = new Intent(this, ActivitySeleccionMesa.class);
         startActivity(actSelMes);
         finish();
+        loadingDialog.dismissDialog();
     }
 
 }
--- app/src/main/java/com/example/sgomesero/ActivitySeleccionMesa.java
+++ app/src/main/java/com/example/sgomesero/ActivitySeleccionMesa.java
@@ -7,9 +7,22 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
+import android.widget.Toast;
+
+import com.androidnetworking.AndroidNetworking;
+import com.androidnetworking.common.Priority;
+import com.androidnetworking.error.ANError;
+import com.androidnetworking.interfaces.JSONObjectRequestListener;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class ActivitySeleccionMesa extends AppCompatActivity {
     private Spinner spin;
+    private String [] ordenes;
+    String [] cadspin;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,52 +31,66 @@ public class ActivitySeleccionMesa extends AppCompatActivity {
         spin = (Spinner)findViewById(R.id.spin_selectmesa);
 
 
-        String [] cadspin = {"Mesa 1","Mesa 2","Mesa 3","Mesa 4","Mesa 5","Mesa 6","Mesa 7"};
-        ArrayAdapter<String> opera = new ArrayAdapter<String>(this,R.layout.spinmesaselct,cadspin);//(Contexto, tipo de spiner,nombre del spin)
-        spin.setAdapter(opera);
+        AndroidNetworking.get("https://safe-bastion-34410.herokuapp.com/api/mesas")
+                .setPriority(Priority.MEDIUM)
+                .build()
+                .getAsJSONObject(new JSONObjectRequestListener() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try {
+                            String respuesta = response.getString("status");
+                            if(respuesta.equals("200")){
+                                JSONArray arrayPlatos = response.getJSONArray("data");
+                                cadspin = new String[arrayPlatos.length()];
+                                for(int i=0;i<arrayPlatos.length();i++){
+                                    JSONObject jsonProducto = arrayPlatos.getJSONObject(i);
+                                    String id = jsonProducto.getString("id");
+                                    cadspin[i]=("Mesa " + id);
+                                }
+                                Llenar();
+                            }else{
+                                Toast.makeText(ActivitySeleccionMesa.this, "No hay ninguna mesa disponible.", Toast.LENGTH_LONG).show();
+                            }
+                        } catch (JSONException e) {
+                            Toast.makeText(ActivitySeleccionMesa.this, "Error: "+e.getMessage(), Toast.LENGTH_LONG).show();
+                        }
+                    }
+
+                    @Override
+                    public void onError(ANError anError) {
+                        Toast.makeText(ActivitySeleccionMesa.this, "Error: "+anError.getErrorDetail(), Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+
+
     }
+
+    public void Llenar(){
+        if(!cadspin.equals(null)) {
+            ArrayAdapter<String> opera = new ArrayAdapter<String>(this, R.layout.spinmesaselct, cadspin);//(Contexto, tipo de spiner,nombre del spin)
+            spin.setAdapter(opera);
+        }
+    }
+
     public void Aceptar(View view){
         String selec, mesa ;
         selec = spin.getSelectedItem().toString();//Selecciona el Item de objeto spinner
-        if(selec.equals("Mesa 1")) {
-            mesa = "Mesa Nro. 1";
-            PasarActivity(mesa);
-        }
-        else if(selec.equals("Mesa 2")) {
-            mesa = "Mesa Nro. 2";
-            PasarActivity(mesa);
-        }
-        else if(selec.equals("Mesa 3")) {
-            mesa = "Mesa Nro. 3";
-            PasarActivity(mesa);
-        }
-        else if(selec.equals("Mesa 4")) {
-            mesa = "Mesa Nro. 4";
-            PasarActivity(mesa);
-        }
-        else if(selec.equals("Mesa 5")) {
-            mesa = "Mesa Nro. 5";
-            PasarActivity(mesa);
-        }
-        else if(selec.equals("Mesa 6")) {
-            mesa = "Mesa Nro. 6";
-            PasarActivity(mesa);
-        }
-        else if(selec.equals("Mesa 7")) {
-            mesa = "Mesa Nro. 7";
-            PasarActivity(mesa);
-        }
+
+        PasarActivity(selec);
     }
     public void PasarActivity(String mesa){
         Intent verificarorden = new Intent(this,ActivityVerificarOrden.class);
         verificarorden.putExtra("m1",mesa);
         startActivity(verificarorden);
-        finish();
+        //finish();
     }
     @Override
     public  void onBackPressed(){
+        /*
         Intent login = new Intent(this,ActivityLogin.class);
         startActivity(login);
+         */
         finish();
     }
 }
