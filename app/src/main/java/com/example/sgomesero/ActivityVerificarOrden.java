e94fa5853fe5a3b939de6d3d232b9179849e49b1
AO <alexortizperugachi@gmail.com>
2020-06-02 01:33:54 -0500

    Activity Verificar Orden Cuadro de Dialogo, presentacion de los platos ingresados en la BDD, nombres de los botones

--- app/src/main/java/com/example/sgomesero/ActivityVerificarOrden.java
+++ app/src/main/java/com/example/sgomesero/ActivityVerificarOrden.java
@@ -1,7 +1,9 @@
 package com.example.sgomesero;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -9,6 +11,15 @@ import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.androidnetworking.AndroidNetworking;
+import com.androidnetworking.common.Priority;
+import com.androidnetworking.error.ANError;
+import com.androidnetworking.interfaces.JSONObjectRequestListener;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
 
@@ -23,13 +34,18 @@ public class ActivityVerificarOrden extends AppCompatActivity {
     private String mes_num;
     private String id_pedido;
 
-    ArrayList<String> listPvp = new ArrayList<>();
-    private String [][] menu,menu2;
+    ArrayList<String> listIdDtall = new ArrayList<>();//Lista de id detalle
+    ArrayList<String> listNomPlts = new ArrayList<>();//Lista de nombre platos
+    ArrayList<String> listCantPlts = new ArrayList<>();//Lista de cantidad platos
+    ArrayList<String> listpvpPlts = new ArrayList<>();//Lista de pvp platos
+    ArrayList<String> listvalorPlts = new ArrayList<>();//Lista de valor platos
+
+    private String [][] menu;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        int i,j;
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_verificar_orden);
 
@@ -43,15 +59,18 @@ public class ActivityVerificarOrden extends AppCompatActivity {
 
         subtitle.setText("Mesa " + mes_num);
 
-        Bundle menumat = getIntent().getExtras();
-        menu2 = (String [][]) menumat.get("menu");//Recibe una matriz ActivitySeleccionOrden
 
         list_orden = (ListView)findViewById(R.id.listview_ordenes);
         res_total = (TextView)findViewById(R.id.txtview_totalres);
+        buscarDetalle();
+        eliminarItemArrayMenu();
 
-
-        menu = new String[1][5];
-        if(menu.length==1 && menu2 == null) {
+    }
+    //Se muestra un menu vacio en el caso de no existir datos en la BDD
+    public void menuVacio(boolean vacio){
+        int i,j;
+        if(vacio) {
+            menu = new String[1][5];
             for (i = 0; i < menu.length; i++) {
                 for (j = 0; j < menu[i].length; j++) {
                     menu[i][0] = "";
@@ -62,13 +81,11 @@ public class ActivityVerificarOrden extends AppCompatActivity {
                 }
             }
         }else
-            verificar();
-        actualizar();
-        eliminarItemArrayMenu();
+            mostrarDetalle();
+        presentarDatos();
     }
-
     //Actualiza la lista en el caso de eliminar platos del menu
-    public void actualizar(){
+    public void presentarDatos(){
         AdapterSelect apt = new AdapterSelect(this,menu);
         list_orden.setAdapter(apt);
 
@@ -79,75 +96,158 @@ public class ActivityVerificarOrden extends AppCompatActivity {
     //Agregar las ordenes del cliente
     public void agregarOrden(View view){
         Intent tipoOrden = new Intent(this, ActivityTipoOrden.class);
+        tipoOrden.putExtra("id_pedido",id_pedido);
+        tipoOrden.putExtra("id_emp",id_emp);
         startActivity(tipoOrden);
     }
     //Ingresar Datos del Cliente
     public void datosFactura(View view){
         Intent factura = new Intent(this, ActivityIngresarDatosFact.class);
-        factura.putExtra("mes_num", mes_num);
+        factura.putExtra("id_pedido",id_pedido);
+        factura.putExtra("id_emp",id_emp);
         startActivity(factura);
-    }
-    public void verificar(){
-        int i,j;
-        menu = new String[menu2.length][5];
-            for (i = 0; i < menu.length; i++) {
-                menu[i][0] =  menu2[i][0];
-                menu[i][1] =  menu2[i][1];
-                menu[i][2] =  menu2[i][2];
-                menu[i][3] =  menu2[i][3];
-                menu[i][4] =  menu2[i][4];
-            }
-        actualizar();
-    }
-    @Override
-    public  void onBackPressed(){
-
         finish();
+    }
+    //Se coloca en la matriz para poder mostrar los datos
+    public void mostrarDetalle(){
+        int i,j;
+        menu = new String[listIdDtall.size()][5];
+            for (i = 0; i < menu.length; i++) {
+                menu[i][0] = listIdDtall.get(i);
+                menu[i][1] = listNomPlts.get(i);
+                menu[i][2] = listCantPlts.get(i);
+                menu[i][3] = listpvpPlts.get(i);
+                menu[i][4] = listvalorPlts.get(i);
+            }
+    }
+    //Buscar en la BDD el datalle de acuerdo al pedido
+    public void buscarDetalle(){
+        String url = "http://safe-bastion-34410.herokuapp.com/api/platopedidos/"+id_pedido;
+        if(!id_pedido.equals("")) {
+            AndroidNetworking.get(url)
+                    .setPriority(Priority.MEDIUM)
+                    .build()
+                    .getAsJSONObject(new JSONObjectRequestListener() {
+                        @Override
+                        public void onResponse(JSONObject response) {
+                            try {
+                                String respuesta = response.getString("status");
+                                if (respuesta.equals("200")) {
+                                    JSONArray arrayPlatos = response.getJSONArray("data");
+                                    for (int i = 0; i < arrayPlatos.length(); i++) {
+                                        JSONObject jsonProducto = arrayPlatos.getJSONObject(i);
+                                        String IdDetalle = jsonProducto.getString("id");
+                                        String nombrePlato = jsonProducto.getString("plt_nom");
+                                        String cantPlato = jsonProducto.getString("dtall_cant");
+                                        String pvpPlato = jsonProducto.getString("plt_pvp");
+                                        String valorPlato = jsonProducto.getString("dtall_valor");
+
+                                        listIdDtall.add(IdDetalle);///Lista ID
+                                        listNomPlts.add(nombrePlato);//Lista Plato
+                                        listCantPlts.add(cantPlato);//Lista descripcion plato
+                                        listpvpPlts.add(pvpPlato);//Lista precio
+                                        listvalorPlts.add(valorPlato);//Lista total plato cantidad
+                                    }
+                                    menuVacio(false);
+                                } else {
+                                    Toast.makeText(ActivityVerificarOrden.this, "No hay ningun plato disponible.", Toast.LENGTH_LONG).show();
+                                    menuVacio(true);
+                                }
+                            } catch (JSONException e) {
+                                Toast.makeText(ActivityVerificarOrden.this, "Error: " + e.getMessage(), Toast.LENGTH_LONG).show();
+                            }
+                        }
+
+                        @Override
+                        public void onError(ANError anError) {
+                            Toast.makeText(ActivityVerificarOrden.this, "Error: " + anError.getErrorDetail(), Toast.LENGTH_SHORT).show();
+                        }
+                    });
+        }
     }
     //Eliminar los Items de la lista
     private void eliminarItemArrayMenu() {
         list_orden.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                String probar;
-                String msm = "Se elimina " + String.valueOf(menu[position][1]);
-                Toast.makeText(getApplicationContext(), msm, Toast.LENGTH_SHORT).show();
-                if (position < menu.length - 1) {
-                    do {
+            public void onItemClick(AdapterView<?> parent, View view, final int position, long lon) {
+                AlertDialog.Builder alerta = new AlertDialog.Builder(ActivityVerificarOrden.this);
+                alerta.setMessage("¿Desea eliminar " + listNomPlts.get(position) + " ?")
+                        .setCancelable(true)
+                        .setPositiveButton("Si", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                Intent activityLogin = new Intent(ActivityVerificarOrden.this, ActivityLogin.class);
+                                eliminaItemBDD(position);
+                                dialog.cancel();
+                            }
+                        })
+                        .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                dialog.cancel();
+                            }
+                        });
 
-                        menu[position][0] = menu[position + 1][0];
-                        menu[position][1] = menu[position + 1][1];
-                        menu[position][2] = menu[position + 1][2];
-                        menu[position][3] = menu[position + 1][3];
-                        menu[position][4] = menu[position + 1][4];
-                        menu[position + 1][0] = "";
-                        menu[position + 1][1] = "";
-                        menu[position + 1][2] = "";
-                        menu[position + 1][3] = "";
-                        menu[position + 1][4] = "";
-                        probar = menu[position + 1][0];
-                        position++;
-                    } while (probar.equals("") && position < menu.length - 1);
-                } else {
-                    menu[position][0] = "";
-                    menu[position][1] = "";
-                    menu[position][2] = "";
-                    menu[position][3] = "";
-                    menu[position][4] = "";
-                }
-                list_orden.deferNotifyDataSetChanged();
-                actualizar();
+                AlertDialog titulo = alerta.create();
+                titulo.setTitle("Eliminar Plato");
+                titulo.show();
             }
 
         });
     }
-    public void QuitarElementos(int position){
+    //Eliman los datos de la base de datos
+    public void eliminaItemBDD(int position){
+        String id = listIdDtall.get(position);
+        String url = "https://safe-bastion-34410.herokuapp.com/api/detalles/"+id;
+
+        JSONObject jsonData = new JSONObject();
+        AndroidNetworking.delete(url)
+                .addJSONObjectBody(jsonData)
+                .setPriority(Priority.MEDIUM)
+                .build()
+                .getAsJSONObject(new JSONObjectRequestListener() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try {
+
+                            String mensaje = response.getString("message");
+                            Toast.makeText(ActivityVerificarOrden.this, mensaje, Toast.LENGTH_SHORT).show();
+                            //Se vacian las listas
+                            listIdDtall.clear();
+                            listNomPlts.clear();
+                            listCantPlts.clear();
+                            listpvpPlts.clear();
+                            listvalorPlts.clear();
+                            menu = null;
+                            buscarDetalle();
+                        } catch (JSONException e) {
+                            Toast.makeText(ActivityVerificarOrden.this, "Error: " + e.getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    }
+
+                    @Override
+                    public void onError(ANError anError) {
+                        Toast.makeText(ActivityVerificarOrden.this, "Error:   " + anError.getErrorDetail(), Toast.LENGTH_SHORT).show();
+                    }
+                });
 
     }
-    public void Confirmar(){
-
+    @Override
+    public  void onBackPressed(){
+        finish();
     }
-    public void Finalizar(){
 
+    @Override
+    public  void onRestart(){
+        super.onRestart();
+        //Se vacian las listas
+        listIdDtall.clear();
+        listNomPlts.clear();
+        listCantPlts.clear();
+        listpvpPlts.clear();
+        listvalorPlts.clear();
+        menu = null;
+
+        buscarDetalle();
     }
 }
